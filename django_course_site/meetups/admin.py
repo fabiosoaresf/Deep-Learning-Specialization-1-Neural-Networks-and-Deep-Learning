4e40d53efd50a63058c36a96fe684777724a46ec
fabiosoaresf <fabiosoaresf@gmail.com>
2022-01-23 18:31:06 -0300

    Continue our Python Django Course 2021. We now have a database, some views, controllers working all right.

--- django_course_site/meetups/admin.py
+++ django_course_site/meetups/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+from .models import Meetup, Location, Participant
+
 # Register your models here.
+
+
+class MeetupAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'location', 'description')
+    list_filter = ('title', 'location', 'date',)
+    prepopulated_fields = {'slug': ('title',)}
+
+
+class ParticipantAdmin(admin.ModelAdmin):
+    list_display = ('email',)
+
+
+admin.site.register(Meetup, MeetupAdmin)
+admin.site.register(Location)
+admin.site.register(Participant, ParticipantAdmin)
--- django_course_site/meetups/views.py
+++ django_course_site/meetups/views.py
@@ -1,23 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 
-
+from .models import Meetup
 # Create your views here.
+
 
 # def starting_page():
 def index(request):
-    meetups = [
-        {
-            'title': 'A First Meetup',
-            'location': 'New York',
-            'slug': 'a-first-meetup'
-        },
-        {
-            'title': 'A Second Meetup',
-            'location': 'Paris',
-            'slug': 'a-second-meetup'
-        }
-    ]
+    # meetups = [
+    #     {
+    #         'title': 'A First Meetup',
+    #         'location': 'New York',
+    #         'slug': 'a-first-meetup'
+    #     },
+    #     {
+    #         'title': 'A Second Meetup',
+    #         'location': 'Paris',
+    #         'slug': 'a-second-meetup'
+    #     }
+    # ]
+
+    meetups = Meetup.objects.all()
+
     return render(request, 'meetups/index.html', {
         'show_meetups': True,
         'meetups': meetups
@@ -25,16 +29,29 @@ def index(request):
 
 
 def meetup_detail(request, meetup_slug):
-    selected_meetup = {
-        'title': 'HARDCODED A First Meetup',
-        'description': 'HARDCODED This is the first meetup'
-        # 'location': 'New York',
-        # 'slug': 'a-first-meetup'
-    }
+    # selected_meetup = {
+    #     'title': 'HARDCODED A First Meetup',
+    #     'description': 'HARDCODED This is the first meetup'
+    #     # 'location': 'New York',
+    #     # 'slug': 'a-first-meetup'
+    # }
+    try:
+        selected_meetup = Meetup.objects.get(slug = meetup_slug)
+        return render(request, 'meetups/meetup-detail.html', {
+            'meetup_found': True,
+            'meetup': selected_meetup
 
-    return render(request, 'meetups/meetup-detail.html', {
-        'selected_meetup': selected_meetup
-    })
+            # 'title': selected_meetup.title,
+            # 'description': selected_meetup.description,
+            # 'slug': selected_meetup.slug
+        })
+    except Exception as exc:
+        return render(request, 'meetups/meetup-detail.html', {
+            'title': '',
+            'description': '',
+            'meetup_found': False,
+        })
+
 
 
 def my_first_json(request):
